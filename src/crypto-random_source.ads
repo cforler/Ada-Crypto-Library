b0e2ce3183527822715237f513854b96fc302c16
macar <macar@macar>
2012-01-09 16:57:19 +0100

    .

--- src/crypto-random_source.ads
+++ src/crypto-random_source.ads
@@ -15,11 +15,11 @@ package Crypto.Random_Source is
   
   procedure Read(This : in Random_Source'Class; Byte_Array : out Bytes);
   
-  procedure Read(This : in Random_Source; B : out B_Block128);-- is abstract;
+  procedure Read(This : in Random_Source; B : out B_Block128) is null;-- is abstract;
   
-  procedure Read(This : in Random_Source; W : out Word);--  is abstract;
-  procedure Read(This : in Random_Source; Word_Array : out Words);-- is abstract;
+  procedure Read(This : in Random_Source; W : out Word) is null;--  is abstract;
+  procedure Read(This : in Random_Source; Word_Array : out Words) is null;-- is abstract;
 
-  procedure Read(This : in Random_Source; D : out DWord);--  is abstract;
-  procedure Read(This : in Random_Source; DWord_Array : out DWords);-- is abstract;
+  procedure Read(This : in Random_Source; D : out DWord) is null;--  is abstract;
+  procedure Read(This : in Random_Source; DWord_Array : out DWords) is null;-- is abstract;
 end Crypto.Random_Source;
