d46f38548ea96b43651b49a4bd43b39ab9ee9426
Christian Forler <christian.forler@uni-weimar.de>
2014-06-02 18:22:42 +0200

    Moved Skein tests from KDF to hash function test suite

--- test/test-suite_hash_function.adb
+++ test/test-suite_hash_function.adb
@@ -1,9 +1,9 @@
+with Test.Skein;
 with Test.SHA1;
 with Test.SHA256;
 with Test.SHA384;
 with Test.SHA512;
 with Test.Whirlpool;
-
 
 package body Test.Suite_Hash_Function is
    use AUnit.Test_Suites;
@@ -13,6 +13,7 @@ package body Test.Suite_Hash_Function is
    Secure_Hash_Algorithm2_256_Test: 	aliased Test.SHA256.SHA_Test;
    Secure_Hash_Algorithm2_384_Test: 	aliased Test.SHA384.SHA_Test;
    Secure_Hash_Algorithm2_512_Test: 	aliased Test.SHA512.SHA_Test;
+   Skein_Test:           		aliased Test.Skein.Skein_Test;
    Whirlpool_Test: 			aliased Test.Whirlpool.SHA_Test;
   
    function Suite return AUnit.Test_Suites.Access_Test_Suite is
@@ -21,8 +22,8 @@ package body Test.Suite_Hash_Function is
       Add_Test(Result'Access, Secure_Hash_Algorithm2_256_Test'Access);
       Add_Test(Result'Access, Secure_Hash_Algorithm2_384_Test'Access);
       Add_Test(Result'Access, Secure_Hash_Algorithm2_512_Test'Access);
+      Add_Test(Result'Access, Skein_Test'Access);   
       Add_Test(Result'Access, Whirlpool_Test'Access);
-      
       
       return Result'Access;
    end Suite;
